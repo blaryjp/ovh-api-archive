c674a5d1079d4b5ae05ba03e392465a1bc207726
jblary <contact@blary.be>
2014-05-10 23:41:18 +0200

    Add check for route params

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,13 @@
 'use strict';
 
-angular.module('apidiffApp').controller('MainCtrl', function ($scope, $routeParams) {
-    $scope.currentApi = $routeParams.api;
-    $scope.currentView = $routeParams.view;
+angular.module('apidiffApp').controller('MainCtrl', function ($scope, $routeParams, $location) {
+
+    if (($routeParams.api && !~['ovh', 'sys', 'ks', 'ra'].indexOf($routeParams.api)) ||
+        ($routeParams.view && !~['archive', 'diff'].indexOf($routeParams.view))) {
+        $location.path('/');
+    } else {
+        $scope.currentApi = $routeParams.api;
+        $scope.currentView = $routeParams.view;
+    }
+
 });
